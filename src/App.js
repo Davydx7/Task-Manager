5fbf66116fbe3c96e16a9e9380913dc953a0f1b1
Davydx7 <oludave0511@gmail.com>
2021-10-17 05:27:36 +0100

    mock Json-server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+/* eslint-disable no-use-before-define */
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
@@ -10,41 +11,40 @@ function App() {
     setShowAdd(!showAdd);
   };
 
-  const [tasks, setTasks] = useState(
-    [
-      {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-      },
-      {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
-      },
-      {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'Feb 5th at 3:30pm',
-        reminder: false,
-      },
-    ],
-  );
+  // Tasks State
+  const [tasks, setTasks] = useState([]);
 
-  // Add Task
+  // get Tasks from server
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromserver = await fetchTasks();
+      setTasks(tasksFromserver);
+    };
+
+    getTasks();
+  }, []);
+
+  // Fetch Tasks from server
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:2000/tasks');
+    const data = await res.json();
+
+    return data;
+  };
+
+  // Add New Task
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 2000) + 1;
-    console.log(id);
     const newTask = { id, ...task };
     setTasks([...tasks, newTask]);
   };
 
   // delete Tasks
-  const deleteTask = (id) => {
-    // console.log('delete', id)
-    setTasks(tasks.filter((task) => task.id !== id));
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:2000/tasks/${id}`, {
+      method: 'DELETE',
+    });
+    // setTasks(tasks.filter((task) => task.id !== id));
   };
 
   // Toggle Reminder
